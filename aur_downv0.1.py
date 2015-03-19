0a26114c72e373f123a15e6ac021a816cf678714
Michał Kern <p0rt-mail@0.pl>
2015-03-19 19:31:11 +0100

    Update aur_downv0.1.py

--- aur_downv0.1.py
+++ aur_downv0.1.py
@@ -1,6 +1,7 @@
-from os import system
+import os
 
-system("mkdir /tmp/AUR_DOWN")
+os.system("rmdir /tmp/.AUR_DOWN")
+os.system("mkdir /tmp/.AUR_DOWN")
 in_pkgs = raw_input( "Type AUR packages names separated by commas without spaces (eg. eggs,milk,cheese): " )
 med_pkgs = in_pkgs.split(',')
 pkgs = []
@@ -10,10 +11,10 @@ for i in med_pkgs:
 
 def pkgs_install():
     for i in pkgs:
-        system( "cd /tmp/AUR_DOWN && wget https://aur.archlinux.org/packages/" + i[0] + i[1] + '/' + i +'/' + i +".tar.gz" )
-        system("cd /tmp/AUR_DOWN && tar xvf " + i +".tar.gz" )
-        system("cd /tmp/AUR_DOWN/"+i +" && makepkg -si")
+        os.system( "cd /tmp/.AUR_DOWN && wget https://aur.archlinux.org/packages/" + i[0] + i[1] + '/' + i +'/' + i +".tar.gz" )
+        os.system("cd /tmp/.AUR_DOWN && tar xvf " + i +".tar.gz" )
+        os.system("cd /tmp/.AUR_DOWN/"+i +" && makepkg -si")
 
 pkgs_install()
-system("rm -r /tmp/AUR_DOWN")
+os.system("rm -r /.tmp/AUR_DOWN")
 
