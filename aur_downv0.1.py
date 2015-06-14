e94e7fde2cd877304cbb23e033a9c44251e6dbf6
Michał Kern <p0rt-mail@0.pl>
2015-06-14 13:11:49 +0200

    Update aur_downv0.1.py
    bugfix, added argv, now it's working

--- aur_downv0.1.py
+++ aur_downv0.1.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 import os
+from sys import argv
 
-os.system("mkdir /tmp/.AUR_DOWN")
+os.mkidr("/tmp/.AUR_DOWN")
+pkgs = argv; del pkgs[0]
 
 def pkgs_install():
-    for i in str(sys.argv):
+    for i in pkgs:
         os.system( "cd /tmp/.AUR_DOWN && wget https://aur.archlinux.org/packages/" + i[0] + i[1] + '/' + i +'/' + i +".tar.gz" )
         os.system("cd /tmp/.AUR_DOWN && tar xvf " + i +".tar.gz" )
         os.system("cd /tmp/.AUR_DOWN/"+i +" && makepkg -si")
 
 pkgs_install()
-os.system("rm -r /.tmp/AUR_DOWN")
+os.system("rm -r /tmp/.AUR_DOWN")
 
