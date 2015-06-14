a7fe16b204b131128d6b66081bb89e5bfb9d73e5
Michał Kern <p0rt-mail@0.pl>
2015-06-14 12:11:13 +0200

    Update aur_downv0.1.py
    added args

--- aur_downv0.1.py
+++ aur_downv0.1.py
@@ -2,16 +2,9 @@
 import os
 
 os.system("mkdir /tmp/.AUR_DOWN")
-in_pkgs = raw_input( "Type AUR packages names separated byspaces (eg. eggs spam foo): " )
-med_pkgs = in_pkgs.split(' ')
-pkgs = []
-for i in med_pkgs:
-    pkgs.append(i)
-del in_pkgs, med_pkgs
-
 
 def pkgs_install():
-    for i in pkgs:
+    for i in sys.argv:
         os.system( "cd /tmp/.AUR_DOWN && wget https://aur.archlinux.org/packages/" + i[0] + i[1] + '/' + i +'/' + i +".tar.gz" )
         os.system("cd /tmp/.AUR_DOWN && tar xvf " + i +".tar.gz" )
         os.system("cd /tmp/.AUR_DOWN/"+i +" && makepkg -si")
