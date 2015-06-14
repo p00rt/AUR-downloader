8ef5b2ed59755da062d6b2f359902bceae461d50
Michał Kern <p0rt-mail@0.pl>
2015-06-14 12:14:09 +0200

    Update aur_downv0.1.py
    bugfix

--- aur_downv0.1.py
+++ aur_downv0.1.py
@@ -4,7 +4,7 @@ import os
 os.system("mkdir /tmp/.AUR_DOWN")
 
 def pkgs_install():
-    for i in sys.argv:
+    for i in str(sys.argv):
         os.system( "cd /tmp/.AUR_DOWN && wget https://aur.archlinux.org/packages/" + i[0] + i[1] + '/' + i +'/' + i +".tar.gz" )
         os.system("cd /tmp/.AUR_DOWN && tar xvf " + i +".tar.gz" )
         os.system("cd /tmp/.AUR_DOWN/"+i +" && makepkg -si")
